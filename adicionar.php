34db1789ab12b33ba42ce2ad0c687ecb4fd86c43
Luiz Eduardo <euardo8@gmail.com>
2021-06-04 16:02:04 -0300

    ADD ALL

---
+++ adicionar.php
@@ -0,0 +1,12 @@
+<h1>Adicionar Usuario</h1>
+<form method="POST" action="adicionar_action.php">
+    <label>
+        Nome: <br/>
+        <input type="text" name="name"/>
+    </label><br/><br/>
+    <label>
+        Email: <br/>
+        <input type="email" name="email"/>
+    </label><br/><br/>
+    <input type="submit" value="Adicionar" />
+</form>
